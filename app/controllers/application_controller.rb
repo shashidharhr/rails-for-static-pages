56da4f23e3870c16c117db0aacbcdbcbef3a17ab
shashidharhr <shashidhar.hr@gmail.com>
2016-02-10 10:58:12 +0530

    fixed the testimonial alignment for phone view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  after_filter :track_action
   protect_from_forgery with: :exception
 
   
+def track_action
+    ahoy.track "Processed #{'static_pages_controller'}##{'index'}", request.filtered_parameters
+  end
+
   # If your model is called User
   def after_sign_in_path_for(resource)
     session["user_return_to"] || root_path
